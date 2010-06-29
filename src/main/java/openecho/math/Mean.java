4da201b02a3fdde15bd3940a9d51b928217c68f7
John Marsden <j.w.marsden@gmail.com>
2010-06-29 16:26:24 +1000

    Updated File Header, refactored utils to have evaluate triggers.

--- src/main/java/openecho/math/Mean.java
+++ src/main/java/openecho/math/Mean.java
@@ -1,26 +1,45 @@
 /**
- * openecho Java-Pack
- */
+ * Copyright (C) 2010 openecho
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 package openecho.math;
 
 import java.lang.reflect.Array;
 
 /**
+ * Mean utility.
  *
  * @author openecho
+ * @version 1.0.0
  */
 public class Mean {
-   public static double calculateMean(double[] vector) {
-        if(vector==null) {
+    /**
+     * Calculates the mean of the provided vector of data.
+     * @param vector data to calculate the mean.
+     * @return mean
+     */
+    public static double evaluate(double[] data) {
+        if(data==null) {
             throw new NullPointerException();
         }
-        int length = Array.getLength(vector);
+        int length = Array.getLength(data);
         if(length == 0) {
             return 0;
         }
         double sum = 0;
         for(int i=0;i<length;i++) {
-            sum += vector[i];
+            sum += data[i];
         }
         return sum/length;
     }
--- src/main/java/openecho/math/StandardDeviation.java
+++ src/main/java/openecho/math/StandardDeviation.java
@@ -1,27 +1,41 @@
 /**
- * openecho Java-Pack
- */
+ * Copyright (C) 2010 openecho
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 package openecho.math;
 
 import java.lang.reflect.Array;
 
 /**
+ * Standard Deviation Utility
  *
  * @author openecho
+ * @version 1.0.0
  */
 public class StandardDeviation {
-    public static double calculateStandardDeviation(double[] vector) {
-        if(vector==null) {
+    public static double evaluate(double[] data) {
+        if(data==null) {
             throw new NullPointerException();
         }
-        int length = Array.getLength(vector);
+        int length = Array.getLength(data);
         if(length < 2) {
             throw new RuntimeException("More than two values are required to calculate a standard deviation");
         }
-        double mean = Mean.calculateMean(vector);
+        double mean = Mean.evaluate(data);
         double sumDifferenceSquared = 0;
         for(int i=0;i<length;i++) {
-            sumDifferenceSquared += Math.pow(vector[i]-mean,2);
+            sumDifferenceSquared += Math.pow(data[i]-mean,2);
         }
         return Math.sqrt(sumDifferenceSquared/(length-1));
     }

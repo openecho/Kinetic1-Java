ed778425caf26d21220bcf5395a8e298eea0763c
openecho <j.w.marsden@gmail.com>
2010-07-14 11:42:57 +1000

    Base 2D Point Implementation

--- src/main/java/openecho/geometry/Point2D.java
+++ src/main/java/openecho/geometry/Point2D.java
@@ -17,14 +17,17 @@
 package openecho.geometry;
 
 import openecho.math.MutableVector;
-import openecho.math.Vector;
-
+import openecho.math.Matrix;
 /**
  *
  * @author openecho
  */
-public class Point2D extends MutableVector {
-    
+public class Point2D extends MutableVector implements Shape {
+
+    public Point2D(double x, double y) {
+        super(new double[] {x,y});
+    }
+
     public Point2D(double[] data) {
         super(data);
     }
@@ -41,5 +44,34 @@ public class Point2D extends MutableVector {
         return data[Y];
     }
 
-    
+    public Point2D getCentroid() {
+        return this;
+    }
+
+    public Point2D getOrigin() {
+        return this;
+    }
+
+    public Point2D[] getPoints() {
+        return new Point2D[] {this};
+    }
+
+    public double getRadius() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Point2D applyShapeTransformation(ShapeTransformation t) {
+        PointMatrix pointMatrix = new PointMatrix();
+        return Point2D.createFromTransformationMatrix(t.multiply(pointMatrix.setPointData(getCentroid())));
+    }
+
+    public static Point2D createFromTransformationMatrix(Matrix transformationMatrix) {
+        if(transformationMatrix.getN() != 1 || transformationMatrix.getM() < 2) {
+            throw new RuntimeException("Cannot Create Point2D from a Matrix unless its 1xM where M >= 2");
+        }
+        Point2D point = null;
+        double[][] data = transformationMatrix.getData();
+        point = new Point2D(data[0][0], data[1][0]);
+        return point;
+    }
 }

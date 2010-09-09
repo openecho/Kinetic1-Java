c8f5948a9ce3ac3ee1e3f22dcced8102086b5a75
John Marsden <j.w.marsden@gmail.com>
2010-09-09 18:05:07 +1000

    Comment on Matrix3F

--- src/main/java/openecho/math/Matrix3F.java
+++ src/main/java/openecho/math/Matrix3F.java
@@ -16,6 +16,8 @@
 package openecho.math;
 
 /**
+ * Performance optimised 3 dimensional float matrix. This class can be mutable
+ * or immutable.
  *
  * @author openecho
  */
@@ -23,6 +25,7 @@ public class Matrix3F extends MatrixF {
     float m00, m01, m02,
           m10, m11, m12,
           m20, m21, m22 = 0;
+
     
     public Matrix3F(float m00, float m01, float m02,
             float m10, float m11, float m12, 

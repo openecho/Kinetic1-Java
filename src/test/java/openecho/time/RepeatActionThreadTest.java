0228b2ee5d2637670d563777e089a867e11c30e8
John Marsden <j.w.marsden@gmail.com>
2010-09-09 12:27:46 +1000

    Update Tests

--- src/test/java/openecho/time/RepeatActionThreadTest.java
+++ src/test/java/openecho/time/RepeatActionThreadTest.java
@@ -47,4 +47,24 @@ public class RepeatActionThreadTest extends TestCase {
         instance.stop();
     }
 
+    public class TestRepeatableAction extends RepeatedAction {
+
+    Timer timer = Timer.getInstance();
+    StopWatch stopWatch = new StopWatch(timer);
+
+    @Override
+    public String getName() {
+        return "TestRepeatableAction";
+
+    }
+
+    @Override
+    protected void handleExecute(long tick) throws ActionException {
+        stopWatch.stop();
+        System.out.println(this.getClass().getName() + " - tick " + tick + ":\t" + Timer.nanoSecondsToReadableMilliSeconds(stopWatch.getDeltaNanoTime()));
+        stopWatch.start();
+    }
+
+    
+}
 }

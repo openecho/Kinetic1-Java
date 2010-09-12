c120ca04d8245f461c4fac6767cbd6d3cd238536
John Marsden <j.w.marsden@gmail.com>
2010-09-12 14:16:22 +1000

    Math update

--- src/main/java/openecho/math/QuickMath.java
+++ src/main/java/openecho/math/QuickMath.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package openecho.math;
 
 import openecho.math.random.MersenneTwisterFast;
@@ -14,7 +13,6 @@ import openecho.math.random.MersenneTwisterFast;
 public class QuickMath {
 
     MersenneTwisterFast mtf = null;
-
     static QuickMath random = null;
 
     private QuickMath() {
@@ -22,7 +20,7 @@ public class QuickMath {
     }
 
     public static QuickMath getInstance() {
-        if(random == null) {
+        if (random == null) {
             random = new QuickMath();
         }
         return random;
@@ -30,5 +28,29 @@ public class QuickMath {
 
     public static double random() {
         return getInstance().mtf.nextDouble();
+    }
+
+    public static float sin(float a) {
+        return (float) Math.sin(a);
+    }
+
+    public static float asin(float a) {
+        return (float) Math.asin(a);
+    }
+
+    public static float cos(float a) {
+        return (float) Math.cos(a);
+    }
+
+    public static float acos(float a) {
+        return (float) Math.acos(a);
+    }
+
+    public static float tan(float a) {
+        return (float) Math.tan(a);
+    }
+
+    public static float atan(float a) {
+        return (float) Math.atan(a);
     }
 }
